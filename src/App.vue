5ad45c3b3bf2e56257edb60c1bf35ba0946f3858
Peter-Vanderhyde <71889138+Peter-Vanderhyde@users.noreply.github.com>
2022-03-07 13:14:51 -0600

    Starting on navigation. Classes not working

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <span>
+    <app-navigation></app-navigation>
+  </span>
 </template>
+
+<script>
+import AppNavigation from '@/components/AppNavigation.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppNavigation
+  }
+}
+</script>
+
 
 <style>
 #app {
---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,26 @@
+<template>
+    <span>
+        <v-app-bar elevation="3" dense app>
+            <v-app-bar-nav-icon class="hidden-md-and-up"></v-app-bar-nav-icon>
+            <router-link to="/">
+                <v-app-bar-title>Pete's Planner</v-app-bar-title>
+            </router-link>
+            <v-spacer></v-spacer>
+            <v-btn class="hidden-sm-and-down" to="/">Home</v-btn>
+            <v-btn class="hidden-sm-and-down" to="/about">About</v-btn>
+        </v-app-bar>
+    </span>
+</template>
+
+<script>
+export default {
+    name: 'AppNavigation'
+}
+</script>
+
+<style scoped>
+a, a:visited {
+    color: black;
+    text-decoration: none;
+}
+</style>
