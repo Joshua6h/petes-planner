98736cb771bc5a4e491619e8fe30e3a103ac3816
Peter-Vanderhyde <71889138+Peter-Vanderhyde@users.noreply.github.com>
2022-03-07 13:22:46 -0600

    Testing navigation

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,14 +1,15 @@
 <template>
     <span>
-        <v-app-bar elevation="3" dense app>
+        <p class="red--text">SOmething</p>
+        <!-- <v-app-bar elevation="3" dense app>
             <v-app-bar-nav-icon class="hidden-md-and-up"></v-app-bar-nav-icon>
             <router-link to="/">
                 <v-app-bar-title>Pete's Planner</v-app-bar-title>
             </router-link>
             <v-spacer></v-spacer>
-            <v-btn class="hidden-sm-and-down" to="/">Home</v-btn>
+            <v-btn class="d-sm-none" to="/">Home</v-btn>
             <v-btn class="hidden-sm-and-down" to="/about">About</v-btn>
-        </v-app-bar>
+        </v-app-bar> -->
     </span>
 </template>
 
