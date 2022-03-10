fe4a433daefdfad0e6ee53a7c7f19cc92a531845
Peter-Vanderhyde <71889138+Peter-Vanderhyde@users.noreply.github.com>
2022-03-09 21:02:24 -0600

    Finishing touches to navigation and colors

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,21 +1,20 @@
 <template>
     <span>
-        <v-toolbar elevation="3">
+        <v-toolbar elevation="3" color="#FCEDDA">
             <v-app-bar-nav-icon class="hidden-md-and-up"></v-app-bar-nav-icon>
             <router-link to="/">
                 <v-app-toolbar-title >Pete's Planner</v-app-toolbar-title>
             </router-link>
-            <p :class="{'red--text':true}">Test text</p>
-            <template>
-                <v-tabs v-model="tab">
+            <v-toolbar flat color="#FCEDDA">
+                <v-tabs slider-color="#EE4E34">
                     <v-tab to="/">Home</v-tab>
                     <v-tab to="/about">About</v-tab>
                 </v-tabs>
-            </template>
+            </v-toolbar>
             <v-spacer></v-spacer>
             <v-btn plain outlined>Sign In</v-btn>
             <v-btn icon>
-                <v-icon>mdi-account</v-icon>
+                <v-icon>mdi-account-circle</v-icon>
             </v-btn>
         </v-toolbar>
     </span>
