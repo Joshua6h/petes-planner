94907f1f0c1abadf28effa94225d01d05ebce456
Peter-Vanderhyde <peter.vanderhyde@gmail.com>
2022-03-30 10:49:11 -0500

    Added email elements and replaced reset password

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -2,6 +2,8 @@
     <v-form @submit.prevent="signUp">
         <v-text-field label="Username" autofocus outlined v-model="username"
                 :color="this.$store.state.accent" background-color="white"></v-text-field>
+        <v-text-field label="Email" outlined type="email" v-model="email"
+                :color="this.$store.state.accent" background-color="white"></v-text-field>
         <v-text-field label="Password" outlined type="password" v-model="password"
                 :color="this.$store.state.accent" background-color="white"></v-text-field>
         <v-text-field label="Re-enter Password" outlined type="password" v-model="secondPassword"
@@ -16,16 +18,18 @@ export default {
     data() {
         return {
             username: '',
+            email: '',
             password: '',
             secondPassword: ''
         }
     },
     methods: {
         signUp() {
-            if(this.username && this.password && this.secondPassword) {
+            if(this.username && this.email && this.password && this.secondPassword) {
                 if(this.password === this.secondPassword){
                     console.log("signed up");
                     console.log(this.username);
+                    console.log(this.email);
                     console.log(this.password);
                     console.log(this.secondPassword);
                     this.$emit('raiseError', '');
