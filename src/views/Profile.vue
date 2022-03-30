605706e714d5b0598a089b481776f0218a1ef7ae
Peter-Vanderhyde <peter.vanderhyde@gmail.com>
2022-03-29 20:06:48 -0500

    Adding buttons to profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,17 +1,21 @@
 <template>
     <div>
         <v-row fluid>
-            <v-col md="5"></v-col>
+            <v-col md="5" class="left-side">
+                <v-btn>Friends</v-btn>
+                <v-btn>Events</v-btn>
+            </v-col>
             <v-col md="2"><profile-image></profile-image></v-col>
             <v-col></v-col>
         </v-row>
         <v-row>
-            <v-col md="5"></v-col>
-            <v-col md="2">
-                <h2>Username</h2>
+            <v-col md="12">
+                <v-container>
+                    <h2>Username <v-btn icon><v-icon>mdi-border-color</v-icon></v-btn></h2>
+                    <h2>Example.email@go.here</h2>
+                </v-container>
                 <v-btn depressed class="mt-10">Logout</v-btn>
             </v-col>
-            <v-col></v-col>
         </v-row>
     </div>
 </template>
@@ -26,3 +30,10 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.left-side {
+    display: flex;
+    align-content: flex-start;
+}
+</style>
